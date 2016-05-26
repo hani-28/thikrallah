efb93c4a3b96871bf3a2f997dcf41b4393ddde61
hani28 <28.hani@gmail.com>
2016-05-26 08:01:35 -0400

    finished athan addition and addition of user defined thikrs with audio

---
+++ app/src/main/java/com/HMSolutions/thikrallah/Fragments/PrefsGeneralFragment.java
@@ -0,0 +1,137 @@
+package com.HMSolutions.thikrallah.Fragments;
+
+import com.HMSolutions.thikrallah.MainActivity;
+import com.HMSolutions.thikrallah.R;
+import com.HMSolutions.thikrallah.Notification.MyAlarmsManager;
+import com.HMSolutions.thikrallah.Utilities.TimePreference;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.util.Log;
+
+public class PrefsGeneralFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener{
+
+
+
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Load the preferences from an XML resource
+		addPreferencesFromResource(R.xml.preferences);
+		initSummary(getPreferenceScreen());
+	}
+	
+	private void updatePrefSummary(Preference pref) {
+		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.getActivity().getApplicationContext());
+		if (pref instanceof ListPreference) {
+			
+			Log.d("prefs","pref is instance of listpreference");
+			ListPreference listPref = (ListPreference) pref;
+			pref.setSummary(listPref.getEntry());
+		}
+		if (pref instanceof TimePreference) {
+			Log.d("prefs","pref is instance of TimePreference");
+			String time=sharedPreferences.getString(pref.getKey(), "00:00");
+			String AMPM="AM";
+			int hour=TimePreference.getHour(time);
+			if (hour>12){
+				hour=hour-12;
+				AMPM="PM";
+			}
+			if (hour==0){
+				hour=12;
+			}
+			String hourString="";
+			if (hour<10){
+				hourString="0"+hour;
+			}else{
+				hourString=""+hour;
+			}
+			int minutes=TimePreference.getMinute(time);
+			String minutesString="";
+			if (minutes<10){
+				minutesString="0"+minutes;
+			}else{
+				minutesString=""+minutes;
+			}
+			pref.setSummary(hourString+":"+minutesString+" "+AMPM);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private void initSummary(PreferenceScreen p) {
+		if (p instanceof PreferenceGroup) {
+			PreferenceGroup pGrp = (PreferenceGroup) p;
+			for (int i = 0; i < pGrp.getPreferenceCount(); i++) {
+				initSummary(pGrp.getPreference(i));
+			}
+		} else {
+			updatePrefSummary(p);
+		}
+
+	}
+	private void initSummary(Preference p) {
+        if (p instanceof PreferenceGroup) {
+            PreferenceGroup pGrp = (PreferenceGroup) p;
+            for (int i = 0; i < pGrp.getPreferenceCount(); i++) {
+                initSummary(pGrp.getPreference(i));
+            }
+        } else {
+            updatePrefSummary(p);
+        }
+    }
+
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+	}
+	@Override
+	public void onPause() {
+		getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+		super.onPause();
+	}
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		MyAlarmsManager manager=new MyAlarmsManager(this.getActivity().getApplicationContext());
+		manager.UpdateAllApplicableAlarms();
+
+		Preference pref = findPreference(key);
+		updatePrefSummary(pref);
+
+        if (key.equalsIgnoreCase("language")){
+            Intent intent=new Intent();
+            intent.setClass(this.getActivity(), MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.putExtra("FromPreferenceActivity",true);
+            this.startActivity(intent);
+        }
+	
+
+	}
+	
+
+
+}
