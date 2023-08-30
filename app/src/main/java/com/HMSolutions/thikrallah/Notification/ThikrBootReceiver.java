207dfe0fca856b739374a263c033a092e1f62dd3
hani-28 <28.hani@gmail.com>
2023-08-30 00:50:53 -0400

    Fixed ForegroundServiceStartNotAllowedException on ThikrbootReciever

--- app/src/main/java/com/HMSolutions/thikrallah/Notification/ThikrBootReceiver.java
+++ app/src/main/java/com/HMSolutions/thikrallah/Notification/ThikrBootReceiver.java
@@ -7,26 +7,33 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
 
 public class ThikrBootReceiver extends BroadcastReceiver {
-    private String TAG="ThikrBootReceiver";
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Log.d(TAG,"ThikrBootReceiver onrecieve called");
 		if (null != intent.getAction()){
 			if (intent.getAction().equalsIgnoreCase("com.HMSolutions.thikrallah.Notification.ThikrBootReceiver.android.action.broadcast")
 					|| intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)){
 				MyAlarmsManager manager=new MyAlarmsManager(context);
 				manager.UpdateAllApplicableAlarms();
 
-				SharedPreferences mPrefs=PreferenceManager.getDefaultSharedPreferences(context);;
+				SharedPreferences mPrefs=PreferenceManager.getDefaultSharedPreferences(context);
 				boolean isTimer=mPrefs.getBoolean("foreground_athan_timer",true);
 
 				if(isTimer){
 					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-						context.startForegroundService(new Intent(context,AthanTimerService.class));
+						if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.S){
+							if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)){
+								context.startForegroundService(new Intent(context,AthanTimerService.class));
+							}
+							//foreground service start not allowed unless at boot completed since SDK 31
+
+
+						}else{
+							context.startForegroundService(new Intent(context,AthanTimerService.class));
+						}
+
 					} else {
 						context.startService(new Intent(context,AthanTimerService.class));
 					}
